2a887807345cd46ddbca982850f8654291438870
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-07 13:24:37 +0530

    Add: UC-5SortSingleDoubleTriple

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-declare -A doublet_combinations
+declare -A singlet_combinations doublet_combinations triplet_combinations
+declare -a sorted_singlet_combinations sorted_doublet_combinations sorted_triplet_combinations
+
 declare -i total_flips=0
 
 flip_coin() {
@@ -18,16 +20,18 @@ read -p "Enter the number of times to flip the coin: " num_flips
 for ((i=1; i<=num_flips; i++)); do
     result1=$(flip_coin)
     result2=$(flip_coin)
-    doublet="$result1$result2"
-    doublet_combinations[$doublet]=$((doublet_combinations[$doublet]+1))
+    result3=$(flip_coin)
+    singlet_combinations[$result1]=$((singlet_combinations[$result1]+1))
+    doublet_combinations["$result1$result2"]=$((doublet_combinations["$result1$result2"]+1))
+    triplet_combinations["$result1$result2$result3"]=$((triplet_combinations["$result1$result2$result3"]+1))
     total_flips+=1
 done
 
-echo "Doublet Combination Percentage:"
+sorted_singlet_combinations=($(echo "${!singlet_combinations[@]}" | tr ' ' '\n' | sort -n))
+sorted_doublet_combinations=($(echo "${!doublet_combinations[@]}" | tr ' ' '\n' | sort -n))
+sorted_triplet_combinations=($(echo "${!triplet_combinations[@]}" | tr ' ' '\n' | sort -n))
 
-for combination in HH TT HT TH; do
-    count=${doublet_combinations[$combination]}
-    percentage=$(bc -l <<< "scale=2; ($count / $total_flips) * 100")
-    echo "$combination: $percentage%"
-done
+echo "Winning Singlet Combination: ${sorted_singlet_combinations[-1]}"
+echo "Winning Doublet Combination: ${sorted_doublet_combinations[-1]}"
+echo "Winning Triplet Combination: ${sorted_triplet_combinations[-1]}"
 
