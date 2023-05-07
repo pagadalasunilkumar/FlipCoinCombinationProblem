c323b127b05664e214c77c39473949c0d9cc7d34
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-07 13:19:13 +0530

    Add: UC-4TripletCombination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare -A doublet_combinations
+declare -A triplet_combinations
 declare -i total_flips=0
 
 flip_coin() {
@@ -18,15 +18,16 @@ read -p "Enter the number of times to flip the coin: " num_flips
 for ((i=1; i<=num_flips; i++)); do
     result1=$(flip_coin)
     result2=$(flip_coin)
-    doublet="$result1$result2"
-    doublet_combinations[$doublet]=$((doublet_combinations[$doublet]+1))
+    result3=$(flip_coin)
+    triplet="$result1$result2$result3"
+    triplet_combinations[$triplet]=$((triplet_combinations[$triplet]+1))
     total_flips+=1
 done
 
-echo "Doublet Combination Percentage:"
+echo "Triplet Combination Percentage:"
 
-for combination in HH TT HT TH; do
-    count=${doublet_combinations[$combination]}
+for combination in HHH HHT HTH THH TTH THT HTT TTT; do
+    count=${triplet_combinations[$combination]}
     percentage=$(bc -l <<< "scale=2; ($count / $total_flips) * 100")
     echo "$combination: $percentage%"
 done
