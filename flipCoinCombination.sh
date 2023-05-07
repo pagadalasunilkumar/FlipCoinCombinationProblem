80e656061b77cce11d010a1145fed849ff7b2722
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-07 13:09:24 +0530

    Add: UC-2MultipleFlipCoin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,14 +1,31 @@
+#!/bin/bash
+
+declare -A singlet_combinations
+declare -i total_flips=0
 
 flip_coin() {
     local result=$((RANDOM % 2))
 
     if ((result == 0)); then
-        echo "Heads"
+        echo "H"
     else
-        echo "Tails"
+        echo "T"
     fi
 }
 
-result=$(flip_coin)
+read -p "Enter the number of times to flip the coin: " num_flips
 
-echo "The result is: $result"
+for ((i=1; i<=num_flips; i++)); do
+    result=$(flip_coin)
+    singlet_combinations[$result]=$((singlet_combinations[$result]+1))
+    total_flips+=1
+done
+
+echo "Singlet Combination Percentage:"
+
+for combination in "${!singlet_combinations[@]}"; do
+    count=${singlet_combinations[$combination]}
+    percentage=$(bc -l <<< "scale=2; ($count / $total_flips) * 100")
+    echo "$combination: $percentage%"
+done
+
